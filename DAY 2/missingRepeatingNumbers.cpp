ee4276c11446c336394d7d3e6e9762914c832847
nikhil vyas <vyasn0123@gmail.com>
2022-04-14 22:36:47 +0530

    Day 2

--- DAY 2/missingRepeatingNumbers.cpp
+++ DAY 2/missingRepeatingNumbers.cpp
@@ -12,3 +12,44 @@ pair<int,int> missingAndRepeating(vector<int> &arr, int n){
     long long int repeating = (long long int)((long long int)missing - (long long int)diff);
     return {(long long int)missing, (long long int)repeating};
 }
+
+// METHOD 2 USING XOR BITWISE
+
+class Solution{
+public:
+    int *findTwoElement(int *arr, int n) {
+         int res=0;
+         for(int i=0;i<n;i++){
+             res=res^arr[i]^(i+1);
+         }
+         int x=res&~(res-1);
+         int res1=0,res2=0;
+         for(int i=0;i<n;i++){
+            
+             if((x&arr[i])==0){
+                 res1=res1^arr[i];
+             }
+             else{
+                 res2=res2^arr[i];
+             }
+             if((x&(i+1))==0){
+                 res1=res1^(i+1);
+             }
+             else{
+                 res2=res2^(i+1);
+             }
+         }
+         int *a=new int[2];
+         for(int i=0;i<n;i++){
+             if(arr[i]==res1){
+                 int t=res2;
+                 res2=arr[i];
+                 res1=t;
+                 break;
+             }
+         }
+         a[0]=res2;
+         a[1]=res1;
+         return a;
+    }
+};
