f5df9491628cff54a8ea548e5e9a06c08fad6732
Nikhil Vyas <65950622+nikhil-sjce@users.noreply.github.com>
2022-09-03 09:32:12 -0700

    Update PascalsTriangle.cpp

--- DAY 1/PascalsTriangle.cpp
+++ DAY 1/PascalsTriangle.cpp
@@ -55,3 +55,22 @@ public:
         }
     }
 };
+
+
+//Method : 2
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>answer(numRows);
+        for(int i=0;i<numRows;i++){
+            answer[i].resize(i+1);
+            answer[i][0] = 1;
+            answer[i][i] = 1;
+            for(int j=1;j<=i-1;j++){
+                answer[i][j] = answer[i-1][j-1] + answer[i-1][j];
+            }
+        }
+        return answer;
+    }
+};
